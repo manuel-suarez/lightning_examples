044a9fb36db922349147367a95f8c7dbceab90aa
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-27 23:42:40 -0600

    Initial setup

---
+++ basic_gan/main.py
@@ -0,0 +1,15 @@
+import os
+
+import lightning as L
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+
+PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
+BATCH_SIZE = 256 if torch.cuda.is_available() else 64
+NUM_WORKERS = int(os.cpu_count() / 2)
